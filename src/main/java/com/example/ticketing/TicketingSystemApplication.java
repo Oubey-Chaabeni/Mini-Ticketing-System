e2ae9f398ec7316918a0bfe700adc147341af7d4
Oubey-Chaabeni <chaabeniouney@gmail.com>
2026-01-28 12:11:51 +0100

    ticketList

--- src/main/java/com/example/ticketing/TicketingSystemApplication.java
+++ src/main/java/com/example/ticketing/TicketingSystemApplication.java
@@ -1,5 +1,8 @@
 package com.example.ticketing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +16,14 @@ public class TicketingSystemApplication {
 		SpringApplication.run(TicketingSystemApplication.class, args);
 	}
 	@GetMapping("/ticket")
-	public Ticket ticket()
+	public List<Ticket> ticket()
 	{
-		return(new Ticket(01,"Freak Party , Epstin Island",20.6969));
+		List<Ticket> ticketList = new ArrayList<>();
+		ticketList.add(new Ticket(01,"Freak Party , Epstin Island",20.6969));
+		ticketList.add(new Ticket(02,"Diddy s white party , Diddy s mansion",45.34));
+		ticketList.add(new Ticket(03,"Lana Del Rey Concert , Feriena",3.00));
+		ticketList.add(new Ticket(04,"The Game Awards , Los Kasserine",25.33));
+		return(ticketList);
 	}
 
 }
