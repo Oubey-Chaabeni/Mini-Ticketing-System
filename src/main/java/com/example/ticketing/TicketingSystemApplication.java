b7b2c0921635ff1502bb4c7a751cf18d1cf3ed88
Oubey-Chaabeni <chaabeniouney@gmail.com>
2026-01-28 13:10:02 +0100

    submit-ticket

--- src/main/java/com/example/ticketing/TicketingSystemApplication.java
+++ src/main/java/com/example/ticketing/TicketingSystemApplication.java
@@ -13,17 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TicketingSystemApplication {
 
+	List<Ticket> ticketList = new ArrayList<>();
 	public static void main(String[] args) {
+		
 		SpringApplication.run(TicketingSystemApplication.class, args);
 	}
 	@GetMapping("/ticket")
 	public Ticket ticket(@RequestParam int key)
 	{
-		List<Ticket> ticketList = new ArrayList<>();
 		ticketList.add(new Ticket(01,"Freak Party , Epstin Island",20.6969));
 		ticketList.add(new Ticket(02,"Diddy s white party , Diddy s mansion",45.34));
 		ticketList.add(new Ticket(03,"Lana Del Rey Concert , Feriena",3.00));
 		ticketList.add(new Ticket(04,"The Game Awards , Los Kasserine",25.33));
+
 		for(Ticket t : ticketList)
 		{
 			if(t.getId()==key)
@@ -32,5 +34,10 @@ public class TicketingSystemApplication {
 			}
 		}
 		return(null);
+	}
+	@GetMapping("/Submiticket")
+	public void st(@RequestParam int key,@RequestParam String name,@RequestParam double price)
+	{
+		ticketList.add(new Ticket(key, name, price));
 	}
 }
